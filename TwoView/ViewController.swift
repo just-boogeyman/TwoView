a072904a03e78387626b8f0942d6ee170cd07d5b
Aroslav Kochkin <kochkin.2328@inbox.ru>
2023-09-10 18:27:05 +0700

    Initial Commit

---
+++ TwoView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoView
+//
+//  Created by Ярослав Кочкин on 10.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
